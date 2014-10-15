6d4f3387b34a2bc0888a2e0ea7f7bea52fb940a4
Thomas Richter <th.richter@uksh.de>
2014-10-15 15:48:34 +0200

    added na=""

--- shuffle.r
+++ shuffle.r
@@ -5,6 +5,6 @@ lapply(files, function(file) {
     if (!grepl(".cfg.", file)) {
 		M <- read.table(file, sep="\t", header=T) # load file
 		M[] <- lapply(M, sample)
-		write.table(M, gsub(".csv_", "_", paste(file, "_shuffled.csv", sep="")), sep="\t", quote=F, row.names=F, col.names=T)
+		write.table(M, gsub(".csv_", "_", paste(file, "_shuffled.csv", sep="")), sep="\t", quote=F, row.names=F, col.names=T, na="")
     }
 })
