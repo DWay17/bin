f36f637595a82ede91e610efa9e4e9f51c96aa67
Thomas Richter <th.richter@uksh.de>
2014-10-29 09:32:44 +0100

    start cmd for talend

---
+++ TOS_DI-win-x86_64.cmd
@@ -0,0 +1,8 @@
+c:
+cd c:\Programme\TOS\TOS_DI-r104014-V5.3.1\
+REM LANG=de_DE.UTF-8
+LANG=en_GB.UTF-8
+echo %DATE% %TIME% >> TOS_DI-win-x86_64.log
+start "" /B TOS_DI-win-x86_64.exe >> TOS_DI-win-x86_64.log
+echo %DATE% %TIME% >> TOS_DI-win-x86_64.log
+
