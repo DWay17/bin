a494755d0c41c1e1a3458f18fbeb3c387b36d2f2
Thomas Richter <th.richter@uksh.de>
2014-10-29 09:32:28 +0100

    timetamp in log

--- ioe.cmd
+++ ioe.cmd
@@ -1,4 +1,6 @@
 REM ioe.cmd
 DIR=C:\Programme\IDRT_IOE\de.umg.mi.idrt.product.id-win32.win32.x86_64\IOE.ini
-cd %DIR%
-start "" /B IOE.exe >> IOE.log
+cd "%DIR%"
+echo %DATE% %TIME% >> IOE.log
+start "ioe" /B IOE.exe >> IOE.log
+echo %DATE% %TIME% >> IOE.log
--- mindterm.cmd
+++ mindterm.cmd
@@ -1,4 +1,6 @@
 REM 
 DIR=c:\Programme\mindterm\
 cd %DIR%
+echo %DATE% %TIME% >> mindterm.log
 start "mindterm" /B java -jar mindterm.jar >> mindterm.log
+echo %DATE% %TIME% >> mindterm.log
