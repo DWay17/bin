03b67c24057a933d869da4feca1388ebeeb6a8e4
Thomas Richter <th.richter@uksh.de>
2014-11-10 14:35:04 +0100

    redirect ERR out to STD out

--- ioe.cmd
+++ ioe.cmd
@@ -2,5 +2,6 @@ REM ioe.cmd
 DIR=C:\Programme\IDRT_IOE\de.umg.mi.idrt.product.id-win32.win32.x86_64\IOE.ini
 cd "%DIR%"
 echo %DATE% %TIME% >> IOE.log
-start "ioe" /B IOE.exe >> IOE.log
+REM start "ioe" /B IOE.exe >> IOE.log
+start "ioe" /B IOE.exe >> IOE.log 2>&1
 echo %DATE% %TIME% >> IOE.log
--- mindterm.cmd
+++ mindterm.cmd
@@ -2,5 +2,7 @@ REM
 DIR=c:\Programme\mindterm\
 cd %DIR%
 echo %DATE% %TIME% >> mindterm.log
-start "mindterm" /B java -jar mindterm.jar >> mindterm.log
+sleep 1
+start "mindterm" /B javaw -jar mindterm.jar >> mindterm.log 2>&1
+sleep 1
 echo %DATE% %TIME% >> mindterm.log
