0b58db4ec0bcd3a8b8a999c55396385ace7ce279
Thomas Richter <th.richter@uksh.de>
2014-11-10 14:34:31 +0100

    doesn't working LANG use -nl en_EN

--- TOS_DI-win-x86_64.cmd
+++ TOS_DI-win-x86_64.cmd
@@ -1,7 +1,7 @@
 c:
 cd c:\Programme\TOS\TOS_DI-r104014-V5.3.1\
 REM LANG=de_DE.UTF-8
-LANG=en_GB.UTF-8
+set LANG=en_GB.UTF-8
 echo %DATE% %TIME% >> TOS_DI-win-x86_64.log
 start "" /B TOS_DI-win-x86_64.exe >> TOS_DI-win-x86_64.log
 echo %DATE% %TIME% >> TOS_DI-win-x86_64.log
