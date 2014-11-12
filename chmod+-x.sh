01a38725334cc21d131b0b3abe4afefea204d922
Thomas Richter <th.richter@uksh.de>
2014-11-12 11:49:54 +0100

    maxdepth as param

--- chmod+-x.sh
+++ chmod+-x.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-find . -maxdepth 1 -type f -regextype posix-extended -regex '.*\.(txt|csv|md|sql|zip|pdf|xlsx?|ini|log|xsl|kdbx?|mail)' -exec chmod -c a-x {} \; 
-find . -maxdepth 1 -type f -regextype posix-extended -regex '.*/#.*#' -exec chmod -c a-x {} \; 
-find . -maxdepth 1 -type f -regextype posix-extended -regex '.*/.*~' -exec chmod -c a-x {} \; 
-find . -maxdepth 1 -type f -regextype posix-extended -regex '.*\.(sh|bash|cmd|bat)' -exec chmod -c ug+x {} \; 
-find . -maxdepth 1 -type d -exec chmod -c ug+x {} \; 
+if [ -n "$1" ]; then
+	MAXDEPTH=$1
+else
+	MAXDEPTH=1
+fi
+find . -maxdepth $MAXDEPTH -type f -regextype posix-extended -regex '.*\.(asc|csv|ini|kdbx?|log|mail|md|pdf|properties|sql|txt|xlsx?|xml|xsl|zip)' -exec chmod -c a-x {} \; 
+find . -maxdepth $MAXDEPTH -type f -regextype posix-extended -regex '.*/#.*#' -exec chmod -c a-x {} \; 
+find . -maxdepth $MAXDEPTH -type f -regextype posix-extended -regex '.*/.*~' -exec chmod -c a-x {} \; 
+find . -maxdepth $MAXDEPTH -type f -regextype posix-extended -regex '.*\.(sh|bash|cmd|bat)' -exec chmod -c ug+x {} \; 
+find . -maxdepth $MAXDEPTH -type d -exec chmod -c ug+x {} \; 
