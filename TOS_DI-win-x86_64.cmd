a8a3ce86e47b8bcf6969ee840972ce557a08f652
Thomas Richter <th.richter@uksh.de>
2014-11-19 10:17:34 +0100

    log also err out

--- TOS_DI-win-x86_64.cmd
+++ TOS_DI-win-x86_64.cmd
@@ -3,6 +3,6 @@ cd c:\Programme\TOS\TOS_DI-r104014-V5.3.1\
 REM LANG=de_DE.UTF-8
 set LANG=en_GB.UTF-8
 echo %DATE% %TIME% >> TOS_DI-win-x86_64.log
-start "" /B TOS_DI-win-x86_64.exe >> TOS_DI-win-x86_64.log
+start "" /B TOS_DI-win-x86_64.exe >> TOS_DI-win-x86_64.log 2>&1
 echo %DATE% %TIME% >> TOS_DI-win-x86_64.log
 
