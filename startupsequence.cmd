df91631d6055baec3bc4cd0e6ad0a77ffb547c00
Thomas Richter <t.richter@ikmb.uni-kiel.de>
2014-12-01 12:53:48 +0100

    StartUpSequence

---
+++ startupsequence.cmd
@@ -0,0 +1,3 @@
+REM startupsequence
+start "" /B C:\Programme\Capture2Text\Capture2Text.exe >> C:\Programme\Capture2Text\Capture2Text.log 2>&1 
+start "" /B c:\Programme\Sizer\sizer.exe >> c:\Programme\Sizer\sizer.log 2>&1
