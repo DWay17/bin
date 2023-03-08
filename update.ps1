c6cae33719a758b677ae8bdc293b18528f7f8d07
Thomas Richter <github.com@thomas-richter.de>
2023-03-08 16:10:26 +0100

    use $env:APPDATA

--- update.ps1
+++ update.ps1
@@ -1,7 +1,7 @@
 # update software
 #Start-Process "C:\Users\Public\Desktop\IObit Software Updater.lnk"
 Start-Process "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\IObit Software Updater\IObit Software Updater.lnk"
-Start-Process "C:\Users\t.richter\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\PatchMyPC.lnk"
+Start-Process "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\PatchMyPC.lnk"
 #echo "choco outdated | sed -e 's/|/\t/g'"
 echo "choco_outdated.ps1"
 choco_outdated.ps1
