85afb890afabf5237258992835d3ddfc05bd0254
Thomas Richter <github.com@thomas-richter.de>
2023-07-06 09:53:58 +0200

    updates for update.ps1

--- update.ps1
+++ update.ps1
@@ -3,10 +3,13 @@
 Start-Process "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\IObit Software Updater\IObit Software Updater.lnk"
 Start-Process "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\PatchMyPC.lnk"
 #echo "choco outdated | sed -e 's/|/\t/g'"
-echo "choco_outdated.ps1"
-choco_outdated.ps1
-echo "winget source update ; winget upgrade"
-winget source update ; winget upgrade
+
+#echo "choco_outdated.ps1"
+#choco_outdated.ps1
+#echo "winget source update ; winget upgrade"
+#winget source update ; winget upgrade
+
+start "C:\Users\t.richter\AppData\Roaming\Microsoft\Windows\Start Menu\WingetUI.lnk"
 
 Import-Module PSWindowsUpdate
 Get-WUList -MicrosoftUpdate
