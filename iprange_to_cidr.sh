d22ae23352b2c4573b66a4d25711604de54cc3d0
Thomas Richter <github.com@thomas-richter.de>
2025-02-10 11:28:29 +0100

    new version

--- iprange_to_cidr.sh
+++ iprange_to_cidr.sh
@@ -1,52 +1,71 @@
 #!/bin/bash
 
-# Function to convert IP address to integer
-ip_to_int() {
-    local a b c d
-    IFS=. read -r a b c d <<< "$1"
-    echo $((a * 256**3 + b * 256**2 + c * 256 + d))
+# Function to convert an IP address to binary
+ip_to_binary() {
+  IFS=. read -r a b c d <<< "$1"
+  printf "%08d%08d%08d%08d" $(bc <<< "obase=2;$a") $(bc <<< "obase=2;$b") $(bc <<< "obase=2;$c") $(bc <<< "obase=2;$d")
 }
 
-# Function to convert integer to IP address
-int_to_ip() {
-    local ip="$1"
-    echo "$((ip >> 24 & 255)).$((ip >> 16 & 255)).$((ip >> 8 & 255)).$((ip & 255))"
-}
-
-# Function to calculate CIDR notation
-range_to_cidr() {
-    local start end size
-
-    start=$(ip_to_int "$1")
-    end=$(ip_to_int "$2")
-
-    while (( start <= end )); do
-        for size in {32..0}; do
-            local mask=$((0xFFFFFFFF << (32 - size) & 0xFFFFFFFF))
-            local masked_start=$((start & mask))
-            if (( masked_start != start || (start + (1 << (32 - size)) - 1 > end) )); then
-                (( size++ ))
-                break
-            fi
-        done
-        echo "$(int_to_ip "$start")/$size"
-        start=$((start + (1 << (32 - size))))
-    done
-}
-
-# Main script
-if [ "$#" -eq 1 ]; then
-    if [[ "$1" == *-* ]]; then
-        IFS=- read -r ip1 ip2 <<< "$1"
-        range_to_cidr "$ip1" "$ip2"
+# Function to convert binary to CIDR notation
+binary_to_cidr() {
+  local binary=$1
+  local length=${#binary}
+  local cidr=0
+  for ((i=0; i<$length; i++)); do
+    if [[ ${binary:$i:1} == 1 ]]; then
+      ((cidr++))
     else
-        echo "$1/32"
+      break
     fi
-elif [ "$#" -eq 2 ]; then
-    ip1="$1"
-    ip2="$2"
-    range_to_cidr "$ip1" "$ip2"
-else
-    echo "Usage: $0 ip1-ip2 or $0 ip1 ip2 or $0 ip"
+  done
+  echo "$cidr"
+}
+
+# Function to convert IP range to CIDR notation
+ip_range_to_cidr() {
+  local start_ip=$1
+  local end_ip=$2
+
+  if [[ "$start_ip" == "$end_ip" ]]; then
+    end_ip="$start_ip"
+  fi
+
+  start_binary=$(ip_to_binary "$start_ip")
+  end_binary=$(ip_to_binary "$end_ip")
+  
+  local cidr
+  while [[ "$start_binary" != "$end_binary" ]]; do
+    start_binary=${start_binary%1*}
+    end_binary=${end_binary%1*}
+  done
+  cidr=$(binary_to_cidr "$start_binary")
+
+  IFS=. read -r a b c d <<< "$start_ip"
+  local base_ip="$a.$b.$c.$d"
+  echo "$base_ip/$cidr"
+}
+
+# Main function
+main() {
+  if [[ "$#" -lt 1 ]]; then
+    echo "Usage: $0 ip_range"
     exit 1
-fi
+  fi
+
+  input="$1"
+  IFS='-' read -r start_ip end_ip <<< "$input"
+  if [[ -z "$end_ip" ]]; then
+    IFS=' ' read -r start_ip end_ip <<< "$input"
+  fi
+  if [[ -z "$end_ip" ]]; then
+    end_ip="$start_ip"
+  fi
+
+  if [[ "$start_ip" == "$end_ip" ]]; then
+    echo "$start_ip/30"
+  else
+    ip_range_to_cidr "$start_ip" "$end_ip"
+  fi
+}
+
+main "$@"
