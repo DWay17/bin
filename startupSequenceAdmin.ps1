09ba0fc5e0351d91e63b6b1a4fb04b50a22edd11
Thomas Richter <github.com@thomas-richter.de>
2025-07-24 10:02:47 +0200

    use run.ps1

--- startupSequenceAdmin.ps1
+++ startupSequenceAdmin.ps1
@@ -1,9 +1,17 @@
 # c:\Users\t.richter\bin\startupsrequence.ps1
+## sizer
 #Stop-Process -Name sizer.exe
 Get-Process | Where-Object {$_.ProcessName -match ".*(sizer).*" } | ForEach-Object { $_.kill() }
-Start-Process "C:\Program Files (x86)\Sizer\sizer.exe"
+#Start-Process "C:\Program Files (x86)\Sizer\sizer.exe"
+run.ps1 sizer
+
+## Capture2Text
 #Stop-Process -Name Capture2Text 
 Get-Process | Where-Object {$_.ProcessName -match ".*(Capture2Text).*" } | ForEach-Object { $_.kill() }
-Start-Process c:\programme\Capture2Text\Capture2Text.exe # ocr
-procexp
+# Start-Process c:\programme\Capture2Text\Capture2Text.exe # ocr
+run.ps1 Capture2Text
+
+## procexp
+Get-Process | Where-Object {$_.ProcessName -match ".*(procexp).*" } | ForEach-Object { $_.kill() }
+run.ps1 procexp
 
