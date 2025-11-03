6d8d47b3c912338f92b8adf5afbd62a2d5570b1d
Thomas Richter <github.com@thomas-richter.de>
2025-11-03 10:49:03 +0100

    minor updates

--- autoruns-disabled.ps1
+++ autoruns-disabled.ps1
@@ -1,31 +1,41 @@
 # 
 param(
-    [string] $Source = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run',
-    [string] $Destination = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run\AutorunsDisabled',
+    [string[]] $Sources = @(
+        'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run',
+        'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run',
+        'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Run';
+    ),
+    [string] $Destination = 'AutorunsDisabled',
     [string] $regexpMatch = '.*(slack|teams).*'
 )
-
-# list all autorun entries in the source registry key maxdepth 1
-$entries = Get-ItemProperty -Path $Source
-#Write-Host "Autorun entries in $Source"
-#Write-Host "$entries"
-# use foreach to iterate through each entry and print its name
-foreach ($entry in $entries.PSObject.Properties) {
-    # skip the default properties
-    if ($entry.Name -match '^(PSPath|PSParentPath|PSChildName|PSDrive|PSProvider)$') { 
-        continue
+foreach ($Source in $Sources) {
+    Write-Host "Processing source key: $Source"
+    # create destination key if it doesn't exist
+    if (-not (Test-Path "$Source\$Destination")) {
+        New-Item -Path "$Source\$Destination" | Out-Null
+        Write-Host "Created destination key: $Source\$Destination"
     }
-    Write-Host -ForegroundColor DarkGray "Name: $($entry.Name), Value: $($entry.Value)"
-    # if name exists in destination overwrite it
-    $existsInDest = Get-ItemProperty -Path $Destination -Name $entry.Name -ErrorAction SilentlyContinue
-    if ($existsInDest) {
-        Write-Host "Overwriting existing entry in destination: $($entry.Name)"    
-        Move-ItemProperty -Path $Source -Name $entry.Name -Destination $Destination -Force
-    } elseif ($entry.Name -match $regexpMatch) {
-        Write-Host -ForegroundColor White "Moving entry matching regex '$regexpMatch': $($entry.Name)"    
-        Move-ItemProperty -Path $Source -Name $entry.Name -Destination $Destination
-    } else {
-        Write-Host -ForegroundColor Gray "Keeping entry in source: $($entry.Name)"    
+    $entries = Get-ItemProperty -Path $Source
+    #Write-Host "Autorun entries in $Source"
+    #Write-Host "$entries"
+    # use foreach to iterate through each entry and print its name
+    foreach ($entry in $entries.PSObject.Properties) {
+        # skip the default properties
+        if ($entry.Name -match '^(PSPath|PSParentPath|PSChildName|PSDrive|PSProvider)$') { 
+            continue
+        }
+        Write-Host -ForegroundColor DarkGray "Name: $($entry.Name), Value: $($entry.Value)"
+        # if name exists in destination overwrite it
+        $existsInDest = Get-ItemProperty -Path "$Source\$Destination" -Name $entry.Name -ErrorAction SilentlyContinue
+        if ($existsInDest) {
+            Write-Host "Overwriting existing entry in destination: $($entry.Name)"
+            Move-ItemProperty -Path $Source -Name $entry.Name -Destination "$Source\$Destination" -Force
+        } elseif ($entry.Name -match $regexpMatch) {
+            Write-Host -ForegroundColor White "Moving entry matching regex '$regexpMatch': $($entry.Name)"
+            Move-ItemProperty -Path $Source -Name $entry.Name -Destination "$Source\$Destination"
+        } else {
+            Write-Host -ForegroundColor Gray "Keeping entry in source: $($entry.Name)"
+        }
     }
+    Write-Host "Finished processing source key: $Source`n"
 }
-
